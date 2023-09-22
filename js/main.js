b4f3d79d5a671992bef85a544b47169b1040b650
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-09-23 00:49:13 +0900

    Add: section8

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ $(document).ready(function () {
   var sliding = false;
 
   $("#fullpage").fullpage({
-    anchors: ["page1", "page2", "page3", "page4"],
+    anchors: [
+      "page1",
+      "page2",
+      "page3",
+      "page4",
+      "page5",
+      "page6",
+      "page7",
+      "page8",
+      "page9",
+      "page10",
+    ],
   });
 });
 
